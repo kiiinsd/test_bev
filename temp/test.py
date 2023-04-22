e981087bc95f266810590b686ad852f150ef5d0b
kiiinsd <2692626645@qq.com>
2023-04-22 16:25:36 +0800

    modified:   temp/test.py

--- temp/test.py
+++ temp/test.py
@@ -1,3 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
 from pyquaternion import Quaternion
-q = Quaternion([1.0, 0.0, 0.0, 0.0])
-print(q.yaw_pitch_roll)
+from projects.PanoSim import PanoSim
+
+pano = PanoSim(data_root='/home/kinsd/test_bev/data/panosim', version='v1.0')
+
+fig = plt.figure()
+
+for sample in pano.sample:
+    lidar_token = sample['data']['LIDAR_TOP']
+    lidar_path, _, _ = pano.get_sample_data(lidar_token)
+    with open(lidar_path, 'r') as f:
+        plt.cla()
+        points = np.fromfile(f, dtype=np.float32).reshape(-1, 5)
+        x, y = points[:, 0], points[:, 1]
+        plt.scatter(x, y, s=5)
+        plt.show()
