853124e14ddddf79223856426993e1021cb7f12c
kiiinsd <2692626645@qq.com>
2025-03-10 20:25:50 +0800

    modified:   projects/bash_runner/vis.sh

--- projects/bash_runner/vis.sh
+++ projects/bash_runner/vis.sh
@@ -1,7 +1,7 @@
 # projects/bash_runner/vis.shlidar_det_pano='projects/tools/visualize.py'
 VIS_PY='projects/tools/visualize.py'
 CONFIG_FILE='projects/configs/bevfusion_lidar_det_pano.py'
-CHECK_POINT='run-d6cb076f-9c530828/latest.pth'
+CHECK_POINT='runs/run-0e6e6bfd-c1daae6d/epoch_3.pth'
 DEBUG_PY='-m debugpy --listen 8531 --wait-for-client'
 
 python ${VIS_PY} ${CONFIG_FILE} --checkpoint ${CHECK_POINT} --mode pred --bbox-score 0.2
